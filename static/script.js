81dd6c9d06c5612cfc7c183947fa46bc88484e8f
Vasiliskkk2 <m69137397@gmail.com>
2024-11-22 09:27:07 +0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -144,6 +144,14 @@ document.addEventListener("touchmove", (event) => {
     });
 });
 
+// Функция для переключения меню
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('active'); // Toggles the active class
+}
+
+
+
 
 
 
